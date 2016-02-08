5ee78ad0fbe5fea482c778a303fb809266513bd3
Dan Vingo <danvingo@gmail.com>
2016-02-07 21:39:16 -0500

    Working on client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import styles from './styles/main.scss'
+import bookData from '../book_data.json'
 
 export default class App extends Component {
   render() {
+    console.log('book data: ', bookData);
+    const books = bookData.map(book => {
+      return <img src={book.cover_url} className="book" />
+    })
     return (
-      <h1>Hello, world NEW PERSON.</h1>
+      <div className="App">
+        {books}
+      </div>
     );
   }
 }
